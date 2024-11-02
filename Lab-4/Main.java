34fae9f2e871590ef749a1c8d6b635d778de5c54
Daniel Kofi Boadu <mrboadu3@gmail.com>
2024-11-01 19:08:37 -0500

    fixed run time between algorithm errors

--- Lab-4/Main.java
+++ Lab-4/Main.java
@@ -2,27 +2,43 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
+
+
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Unsorted Array ---------------------------------------------------");
-      ArrayList<Integer> integerList = Lab4.getList();
-      Lab4.outputList(integerList);
+        System.out.println("Unsorted Array ---------------------------------------------------");
+        ArrayList<Integer> integerList = Lab4.getList();
+        Lab4.outputList(integerList);
 
-      System.out.println("\n\nBubble sort results ----------------------------------------------");
-      ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(integerList);
-      Lab4.outputList(bubbleSortedList);
+        System.out.println("\n\nBubble sort results ----------------------------------------------");
+        ArrayList<Integer> bubbleSortedList = Lab4.bubbleSort(new ArrayList<>(integerList)); // Create a copy to maintain the original list
+        Lab4.outputList(bubbleSortedList);
 
-      System.out.println("\n\nInsertion sort results -------------------------------------------");
-      ArrayList<Integer> insertionSortedList = Lab4.bubbleSort(integerList);
-      Lab4.outputList(insertionSortedList);
+        System.out.println("\n\nInsertion sort results -------------------------------------------");
+        ArrayList<Integer> insertionSortedList = Lab4.insertionSort(new ArrayList<>(integerList)); // Create a copy to maintain the original list
+        Lab4.outputList(insertionSortedList);
+
+        // Measure time for insertion sort
+        ArrayList<Integer> insertionList = new ArrayList<>(integerList);
+        long startInsertion = System.nanoTime();
+        Lab4.insertionSort(insertionList);
+        long endInsertion = System.nanoTime();
+        System.out.println("\nInsertion Sort took: " + (endInsertion - startInsertion) / 1_000_000.0 + " ms");
+
+        // Measure time for bubble sort
+        ArrayList<Integer> bubbleList = new ArrayList<>(integerList);
+        long startBubble = System.nanoTime();
+        Lab4.bubbleSort(bubbleList);
+        long endBubble = System.nanoTime();
+        System.out.println("Bubble Sort took: " + (endBubble - startBubble) / 1_000_000.0 + " ms");
     }
 }
 
 class Lab4 {
-  public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
-    // Step 1 - Implement insertion sort algorithm here
-      for (int i = 1; i < integerList.size(); i++) {
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> integerList) {
+        for (int i = 1; i < integerList.size(); i++) {
             int key = integerList.get(i);
             int j = i - 1;
 
@@ -32,45 +48,41 @@ class Lab4 {
             }
             integerList.set(j + 1, key);
         }
+        return integerList;
+    }
 
-    return integerList;
-  }
-
-  public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
-    // Step 2 - Implement the bubble sort algorithm here
-   for (int i = 0; i < integerList.size() - 1; i++) {
-        for (int j = 1; j < integerList.size() - i; j++) {
-            if (integerList.get(j) < integerList.get(j - 1)) {
-                int temp = integerList.get(j - 1);
-                integerList.set(j - 1, integerList.get(j));
-                integerList.set(j, temp);
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> integerList) {
+        for (int i = 0; i < integerList.size() - 1; i++) {
+            for (int j = 1; j < integerList.size() - i; j++) {
+                if (integerList.get(j) < integerList.get(j - 1)) {
+                    int temp = integerList.get(j - 1);
+                    integerList.set(j - 1, integerList.get(j));
+                    integerList.set(j, temp);
+                }
             }
         }
+        return integerList;
     }
-    return integerList;
-  }
 
-  public static ArrayList<Integer> getList() {
-    ArrayList<Integer> integerList = new ArrayList<>();
-    String line;
-    try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
-        while ((line = br.readLine()) != null) {
-            integerList.add(Integer.parseInt(line));
+    public static ArrayList<Integer> getList() {
+        ArrayList<Integer> integerList = new ArrayList<>();
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader("integers.txt"))) {
+            while ((line = br.readLine()) != null) {
+                integerList.add(Integer.parseInt(line));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
+        return integerList;
     }
-    return integerList;
-  }
 
-  public static void outputList(ArrayList<Integer> integerList) {
-    for (int i = 0; i < integerList.size(); i++) {
-        System.out.print(integerList.get(i) + " ");
+    public static void outputList(ArrayList<Integer> integerList) {
+        for (int i = 0; i < integerList.size(); i++) {
+            System.out.print(integerList.get(i) + " ");
+        }
     }
-  }
 }
-
-
 
  // 1. If you were implementing a sort algorithm for a new language, which sort would you use?
     // Answer: If I were to implement a sort algorithm for a new language, I would use the Merge Sort or Quick Sort. 
